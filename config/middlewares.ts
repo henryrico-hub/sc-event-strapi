4b093ef611a682ddd60230d51adf719ec79c4527
Henry R <henry.ricoe@gmail.com>
2025-07-02 22:40:17 -0700

    update: add SCP to allow image from cloudinary

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,25 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          "default-src": ["'self'"],
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "script-src": ["'self'", "blob:"],
+          "style-src": ["'self'", "'unsafe-inline'"],
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
